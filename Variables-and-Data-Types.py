5741d5bb8df2b419cf3bb660b7d9c00d7cd42907
Rout Biraja Prasad <68429941+Biraja007@users.noreply.github.com>
2020-07-18 18:35:12 +0530

    Add files via upload
    This is for understanding variables and data types in Python.

---
+++ Variables-and-Data-Types.py
@@ -0,0 +1,31 @@
+""" Variables & Data Types:
+
+Variables are reserved memory locations used to store values and are referenced by a name.
+
+No other special character except '_' can be part of a variable name.
+
+Syntax : VariableName = Value
+
+print() is used to give a space in lines of output"""
+
+My_Score = 100
+print (My_Score)
+print()
+#Changing value of a variable:
+MyNumber = 123
+print (MyNumber)
+print()
+MyNumber = 456
+print (MyNumber)
+print()
+#Assigning multiple values to variable:
+a , b , c = 12 , 13.25 , "Hey"
+print(a)
+print()
+print(b)
+print()
+print(c)
+print()
+
+"""Different Data Types in Python are:
+1.List , 2.Numbers , 3.Tuple , 4.Strings , 5.Dictionary"""
