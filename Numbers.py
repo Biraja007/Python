e772ca57896a4f8e5aadcbe08d24caeffcda19c4
Rout Biraja Prasad <68429941+Biraja007@users.noreply.github.com>
2020-07-19 20:50:10 +0530

    Add files via upload
    This file contains explanation about "Numbers" in Python.

---
+++ Numbers.py
@@ -0,0 +1,16 @@
+""""Number is a mathematical object used to count,measure and label.
+Different types of numbers are:Float & Integer"""
+FloatingNumber = 10.25
+
+IntegerNumber = 50
+#Checking type of the Integer
+#Syntax.. type (VariableName)
+print(type(FloatingNumber))
+print()
+
+print(type(IntegerNumber))
+print()
+#Type conversion of Numbers:
+#Syntax.. VariableName = TypeExpected (VariableName)
+FloatingNumber2 = int (FloatingNumber)
+print(FloatingNumber2)
