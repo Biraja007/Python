b379e480f2d738c651922d9f9c92cee2953f6561
Rout Biraja Prasad <68429941+Biraja007@users.noreply.github.com>
2020-07-20 20:14:12 +0530

    Add files via upload
    Here you will have a basic idea of different types of Operators available in Python.

---
+++ Operators.py
@@ -0,0 +1,38 @@
+#   Operator is an action performed between two operands.
+#They are of 4 types:Assignment,Arithmetic,Logical,Relational/Coparision.
+#Assignment operator is used to assign values & denoted by "=" .
+a = 99
+
+#Arithmetic operators are +,-,*,/
+#Addition (+):Returns Sum
+print (2+3)
+#Subtraction (-):Returns difference
+print (5-2)
+#Division(/):Returns quotient
+print (10/2)
+#Multiplication (*):Returns product
+print (2*3)
+#Modulo(%):Returns remainder
+print (10%3)
+
+#Relational/Comparision operators are ==,>,<,<=,>=,!=
+#Equal to (==):Returns true is both LHS=RHS
+print (2==2)
+#Greater than (>):Returns true if operand1 is greater than operand2
+print (5>2)
+#Lesser than (<):Returns true if operand1 is lesser than operand2
+print (5<8)
+#Greater than or equal to (>=):Returns true if operand1 is greater than or equal to operand2
+print (5>=1)
+#\#Lesser than or equal to (<=):Returns true if operand1 is lesser than or equal to operand2
+print (2<=5)
+#Not equal to (!=):Returns true is LHS & RHS are not same
+print (2!=5)
+
+#Logical operators are and,or,#
+#and:Returns true only if both operands are true
+print ((5>3) and (3<7))
+#or:Returns true if atleast any any one is true
+print ((5<3) or (3<5))
+#not:Reverses the boolean value of the operand
+print (not (5<3))
