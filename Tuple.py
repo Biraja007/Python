676a24e3ba359bfbba76584c466cf4afb1a7fc23
Rout Biraja Prasad <68429941+Biraja007@users.noreply.github.com>
2020-07-21 20:33:18 +0530

    Add files via upload
    Here you can understand what is meant by a Tuple.

---
+++ Tuple.py
@@ -0,0 +1,15 @@
+#Tuple is a container that holds many objects under a single name.
+#Tuple is immutable; means it cannot be modified.
+#Syntax: TupleName = (Object1 , Object2 , Object3)
+DateOfBirth = ("01-01-2016" , "13-06-1998" , "14-04-1894")
+print (DateOfBirth)
+
+#Tuple can be accesed by specifying the index of that value.
+DateOfBirth [1]
+
+#Tuple can be deleted using 'del' keyword.
+MyNumber = ("123","456","789")
+print (MyNumber)
+del (MyNumber)
+print (MyNumber)
+#You will get a error as the tuple is deleted at last.
