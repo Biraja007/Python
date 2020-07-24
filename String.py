b06c2e0d331a5bad774a0b6b002168c04bcb8df5
Rout Biraja Prasad <68429941+Biraja007@users.noreply.github.com>
2020-07-24 19:03:39 +0530

    Add files via upload
    Here you can learn more about string and its operation in Python.

---
+++ String.py
@@ -0,0 +1,42 @@
+# String is a sequence of characters that are enclosed in single or double quotes.
+# Syntax: StringName = 'String'
+FruitName = 'Mango Is King Of Fruits'
+print (FruitName)
+print()
+
+# String operations:
+# Upper: Converts all characters to uppercase.
+# Synatx: StringName.upper()
+print ( FruitName.upper() )
+print ()
+
+# Lower: Converts all characters to lowercase.
+# Synatx: StringName.lower()
+print ( FruitName.lower() )
+print ()
+
+# Replace: Used to replace a sequence with updated sequence.
+# Syntax: StringName.replace ('Old sequence' , 'New sequence')
+print ( FruitName.replace ('King' , 'Best') )
+print ()
+
+#  Slicing: Returns a slice of string based on index passed.
+# Syntax: StringName [StartingIndex : EndingIndex]
+print ( FruitName [0:11] )
+print ()
+# If nothing is mentioned the whole string is printed.
+print ( FruitName [:] )
+print ()
+# Similarly if starting index is not mentioned, characters starting from 0 will be printed.
+print ( FruitName [:15] )
+print ()
+# Similarly if endind index is not mentioned, characters from starting index to end will be printed.
+print ( FruitName [13:] )
+print ()
+
+# len () returns the length of a string.
+print ( len (FruitName) )
+print ()
+
+# String formatting in Python is used if one need one string in betwwen of another.
+print ('According to me: {}'.format(FruitName) )
